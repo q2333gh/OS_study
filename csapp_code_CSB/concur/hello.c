4a45495af411318ccb94ffb873d8e51bae48caf2
btwl <1253102853@qq.com>
2023-12-13 13:40:13 +0800

    thread communicate

--- csapp_code_CSB/concur/hello.c
+++ csapp_code_CSB/concur/hello.c
@@ -4,18 +4,35 @@
 /* $begin hello */
 #include "../include/csapp.h"
 void *thread(void *vargp);  // line:conc:hello:prototype
+struct thread_para {
+  int a;
+  char b[32];
+};
 
+long thread_return;  
 int main()  // line:conc:hello:main
 {
   pthread_t tid;                             // line:conc:hello:tid
-  Pthread_create(&tid, NULL, thread, NULL);  // line:conc:hello:create
-  Pthread_join(tid, NULL);                   // line:conc:hello:join
+//   after create. the thread will immeadiately start to run.
+
+  struct thread_para my_para;
+  my_para.a = 2;
+  strcpy(my_para.b, "Hello, thread!");
+  pthread_create(&tid, NULL, thread,
+                 (void *)&my_para);  // line:conc:hello:create
+  //   wait for thread to finishing job.
+  pthread_join(tid, &thread_return);  // line:conc:hello:join
+  printf("Thread returned: %ld\n", (long)thread_return);
   exit(0);                                   // line:conc:hello:exit
 }
 
 void *thread(void *vargp) /* thread routine */  // line:conc:hello:beginthread
 {
-  printf("Hello, world!\n");
-  return NULL;  // line:conc:hello:return
+  struct thread_para *my_para = (struct thread_para *)vargp;
+  printf("Received parameters: a = %d, b = %s\n", my_para->a, my_para->b);
+  printf("Hello, world!\nand simulating exec 3 seconds of computation\n");
+  sleep(3);
+  long thread_return = my_para->a*10;      // Example return value
+  return (void *)thread_return;  // line:conc:hello:return
 }  // line:conc:hello:endthread
 /* $end hello */

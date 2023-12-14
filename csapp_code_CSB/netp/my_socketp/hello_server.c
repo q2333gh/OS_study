80608288f49e46b93cb4ca0d90632c83133a39f5
btwl <1253102853@qq.com>
2023-12-14 22:40:18 +0800

    reaname

--- csapp_code_CSB/netp/my_socketp/hello_server.c
+++ csapp_code_CSB/netp/my_socketp/hello_server.c
@@ -6,44 +6,43 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// also using inline function is ok.
-// similar to macros, but they have the advantages of type checking, debugging,
-// and namespace.
 #define clear_mem_region(obj) (memset(&(obj), 0, sizeof(obj)))
+#define try(expression) \
+  if ((expression) < 0) { \
+    perror(#expression " failed"); \
+    exit(EXIT_FAILURE); \
+  }
 
 int main() {
+  
+  //**** the comments functions is most improtant execution. others statements
+  //just
+  // ****collect nessesary data ****
+
   // socket()
+  // create a socket object instance.
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("socket creation failed");
-    exit(EXIT_FAILURE);
-  }
+  try(sockfd);
+
   // bind()
+  // OS give the socket FD the local address ADDR
   struct sockaddr_in serv_addr;
   clear_mem_region(serv_addr);
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
   serv_addr.sin_port = htons(7000);
-  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
+  try(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
 
   // listen()
-  if (listen(sockfd, 5) < 0) {
-    perror("listen failed");
-    exit(EXIT_FAILURE);
-  }
+  try(listen(sockfd, 5));
 
   // accept()
   struct sockaddr_in cli_addr;
   socklen_t cli_addr_len = sizeof(cli_addr);
   int connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &cli_addr_len);
-  if (connfd < 0) {
-    perror("accept failed");
-    exit(EXIT_FAILURE);
-  }
+  try(connfd);
+
   // write()
   char *message = "Hello world!";
   write(connfd, message, strlen(message));
@@ -52,5 +51,4 @@ int main() {
   // clear()
   close(sockfd);
   return 0;
-  
 }
